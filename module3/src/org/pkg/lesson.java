0c31a4cdb0c1669f4e027e641fb6e7ca27f82812
lsm <spacembg@gmail.com>
2017-04-24 22:00:18 +0300

    test

--- module3/src/org/pkg/lesson.java
+++ module3/src/org/pkg/lesson.java
@@ -1,8 +1,27 @@
 package org.pkg;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by lsm on 4/24/2017.
  */
 public class lesson {
+    public static void main(String[] args) {
+        String[] laArrayY = null;
+        String[] laArrayU = null;
 
+        //System.arraycopy(args, 0, laArrayY, 0, 4);
+        //System.arraycopy(args, 4, laArrayU, 0, 2);
+        laArrayY = Arrays.copyOfRange(args,0, 4);
+        laArrayU = Arrays.copyOfRange(args,4, 6);
+        System.out.println(Arrays.toString(args));
+        System.out.println(Arrays.toString(laArrayY));
+        System.out.println(Arrays.toString(laArrayU));
+        print("Hi I'm calling method print");
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
 }
