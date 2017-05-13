18a9aff517babebdd26b2d1aa142642e4af7ec49
vudshi work <spacemb@mail.ru>
2017-05-13 07:56:58 +0300

    20170511

---
+++ module0/src/itea/org/book/samples/dummy.java
@@ -0,0 +1,34 @@
+package itea.org.book.samples;
+
+
+/**
+ * Created by lsm on 12.05.2017.
+ */
+public class dummy {
+    public static void main(String[] args) {
+        int a = 1;
+        boolean b = true;
+     outer:
+     while (true) {
+         mark:
+         switch (a) {
+             case 0:
+                 System.out.println("switch 0;");
+                 a = 3;
+                 continue mark;
+             case 1:
+                 System.out.println("switch 1;");
+             case 2:
+                 System.out.println("switch 2;");
+             case 3:
+                 System.out.println("switch 3;");
+                 break outer;
+             default:
+                 System.out.println("switch default;");
+                 break outer;
+         }
+     }
+    }
+
+
+}
---
+++ module0/src/itea/org/book/samples/sample1.java
@@ -0,0 +1,20 @@
+package itea.org.book.samples;
+
+/**
+ * Created by lsm on 12.05.2017.
+ */
+public class sample1 {
+    public static void main(String[] args) {
+        Tank tank1 = new Tank();
+        Tank tank2 = new Tank();
+        tank1.level = 9;
+
+    }
+
+}
+class Letter{
+    char c;
+}
+class Tank{
+    int level;
+}
